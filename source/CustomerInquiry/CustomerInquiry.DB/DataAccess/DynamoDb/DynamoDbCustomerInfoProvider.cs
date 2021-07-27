3ae6f116c57cc3f27585642b08112a81e62ce852
irynakmet <iryna.kmet@eleks.com>
2021-07-28 02:19:40 +0300

    added dynamo db interface implementation

---
+++ source/CustomerInquiry/CustomerInquiry.DB/DataAccess/DynamoDb/DynamoDbCustomerInfoProvider.cs
@@ -0,0 +1,22 @@
+﻿using System.Threading.Tasks;
+using Amazon.DynamoDBv2;
+using CustomerInquiry.Common.DTO;
+using CustomerInquiry.Common.Interfaces;
+
+namespace CustomerInquiry.Infrastructure.DataAccess.DynamoDb
+{
+    public class DynamoDbCustomerInfoProvider : ICustomerInfoProvider
+    {
+        private readonly IAmazonDynamoDB _amazonDynamoDb;
+
+        public DynamoDbCustomerInfoProvider(IAmazonDynamoDB amazonDynamoDb)
+        {
+            this._amazonDynamoDb = amazonDynamoDb;
+        }
+
+        public Task<Customer> GetRecentCustomerTransactions(CustomerInquiryCriteria customer)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
